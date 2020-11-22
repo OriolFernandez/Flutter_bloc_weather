e81f35b078859c5069790e04f90418692d230ff4
Oriol Fernandez <oriol@metropolis-lab.io>
2020-11-22 21:35:54 +0100

    block implementation

---
+++ android/app/src/main/kotlin/com/uriolus/flutter_weather/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.uriolus.flutter_weather
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/weather_event.dart
@@ -0,0 +1,23 @@
+part of 'weather_bloc.dart';
+
+@immutable
+abstract class WeatherEvent extends Equatable {
+  const WeatherEvent();
+}
+
+class GetWeather extends WeatherEvent {
+  final String cityName;
+
+  const GetWeather(this.cityName);
+
+  @override
+  List<Object> get props => [cityName];
+}
+class GetDetailWeather extends WeatherEvent {
+  final String cityName;
+
+  const GetDetailWeather(this.cityName);
+
+  @override
+  List<Object> get props => [cityName];
+}
